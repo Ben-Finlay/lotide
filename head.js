648fc9bbfd24fd627c5b56c8a4a2cbcc605316e0
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-04 15:31:09 -0400

    Head makes use of the assertEqual function to compare first element in an array with expected output

---
+++ head.js
@@ -0,0 +1,19 @@
+
+const assertEqual = function(actual, expected) {
+  let output = "";
+  
+  if (actual === expected) {
+    output = `🟢 Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    output = `🔴 Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(output);
+};
+
+const head = function(array){
+  let actual = array[0];
+  return actual;
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
