398e46a4eafa18d633a3afe05ebddd8ca4e61c91
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-04 15:33:07 -0400

    delinted head.js

--- head.js
+++ head.js
@@ -10,10 +10,10 @@ const assertEqual = function(actual, expected) {
   console.log(output);
 };
 
-const head = function(array){
+const head = function(array) {
   let actual = array[0];
   return actual;
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
