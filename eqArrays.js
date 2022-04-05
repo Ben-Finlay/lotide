6da9ad18b39eda1164f0d96b17513712c82c248a
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-05 15:34:03 -0400

    Adding a function to compare 2 arrays

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  let output = "";
+  
+  if (actual === expected) {
+    output = `🟢 Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    output = `🔴 Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(output);
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
+    return true;
+    
+  }
+  return false;
+};
+  
+
+
+// Test Code
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), true);
