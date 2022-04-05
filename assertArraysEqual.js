e085d12bc9217523afeb7800cedc1fef691bee6c
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-05 16:54:59 -0400

    adding without.js to compare and remove values from arrays and updating assertArraysEqual to match requirements.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,18 +2,26 @@ const assertArraysEqual = function(arr1, arr2) {
 
   let output = "";
 
-  if (arr1.length === arr2.length && arr1.every((index, value) => index === arr2[value])) {
-    output = `🟢 Assertion Passed: ${arr1} === ${arr2}`
+  if (eqArrays(arr1, arr2)) {
+    output = `🟢 Assertion Passed: ${arr1} === ${arr2}`;
     
   } else {
     output = `🔴 Assertion Failed: ${arr1} !== ${arr2}`;
     
   }
-  console.log(output)
+  console.log(output);
   return output;
   
 };
 
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length && arr1.every((index, value) => index === arr2[value])) {
+    return true;
+    
+  }
+  return false;
+};
+  
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
