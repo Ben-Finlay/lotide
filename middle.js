7ec28087d88cf7b701b868dacb6d933d44a622e1
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-06 09:41:37 -0400

    adding a function to return the middle value or values of an array

---
+++ middle.js
@@ -0,0 +1,56 @@
+const assertArraysEqual = function(arr1, arr2) {
+
+  let output = "";
+
+  if (eqArrays(arr1, arr2)) {
+    output = `🟢 Assertion Passed: ${arr1} === ${arr2}`;
+    
+  } else {
+    output = `🔴 Assertion Failed: ${arr1} !== ${arr2}`;
+    
+  }
+  console.log(output);
+  return output;
+  
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length && arr1.every((index, value) => index === arr2[value])) {
+    return true;
+    
+  }
+  return false;
+};
+  
+
+const middle = function(array) {
+  middleArray = [];
+  midval = 0;
+  if (array.length === 1 || array.length === 2 ) {
+    console.log(middleArray)
+    return middleArray;
+  } else if (array.length % 2 !== 0) {
+    midval = Math.floor(array.length / 2)
+    middleArray.push(array[midval]);
+    console.log(middleArray)
+    return middleArray
+  } else {
+    midval = Math.floor(array.length / 2)
+    middleArray.push(array[midval - 1]);
+    middleArray.push(array[midval]);
+    console.log(middleArray)
+    return middleArray;
+  }
+  }
+
+
+
+
+assertArraysEqual(middle([1, 2, 3, 4, 5], [3]))
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+middle(['a'])
+middle(['b', 'c'])
+middle(['a', 'b', 'c'])
+middle([1, 2, 3, 4, 5])
+middle([6, 7, 8, 9, 10, 11])
+middle([1, 2, 3, 4, 5, 6])
