2083811a845f1171305ccf15a7dd8f2c66d3e1a2
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-07 14:40:48 -0400

    Find a value in an object in an object and return the related key.

---
+++ findKey.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  let output = "";
+  
+  if (actual === expected) {
+    output = `🟢 Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    output = `🔴 Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(output);
+};
+
+const findKey = function(obj, callback) {
+  //iterates through the parent object keys
+  for (let i in obj) {
+    //if callback value is true for child object value
+    if (callback(obj[i])) {
+      //return parent object.
+      return i;
+    }
+  }
+};
+
+
+//Example
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+//Comparison Test
+
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma")); // => "noma"
