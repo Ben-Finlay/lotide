2c54c992edb6293619bd8d5c329ace01f40465fd
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-11 13:47:44 -0400

    Added Mocha & Chai for testing. Revised headTest.js to reflect this and make use.

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,22 @@
-const assertEqual = require('../assertEqual');
+let chai = require('chai');
+let assert = chai.assert;
+
 const head = require('../head');
 
+
+
 //Test Code
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("Number test: returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  
+  it("String test: returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+});
